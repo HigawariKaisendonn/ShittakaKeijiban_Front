8c1cfeab3d99d0c90cf9df0c260c2ae037b9b000
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-06-09 09:47:17 +0900

    scss修正

--- web/src/components/organisms/AuthForm/index.tsx
+++ web/src/components/organisms/AuthForm/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import './AuthForm.css';
+import './AuthForm.scss';
 
 export const AuthForm: React.FC = () => {
   const [isSignUp, setIsSignUp] = useState(false);
