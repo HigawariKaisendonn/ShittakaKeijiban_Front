e50543dd31dfbbef740683b2db1b2f492ed436d9
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-06-27 08:55:59 +0900

    Fix: resolved merge conflict in Header.tsx

--- web/src/components/molecules/header/Header.tsx
+++ web/src/components/molecules/header/Header.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { Icon } from '@/components/atoms/icon/Icon';
-import { AuthButton } from '../authButton/authButton';
+import { Menu } from 'lucide-react';
+import { AuthButton } from '../authButton/AuthButton';
 import './header.scss'
 
 export const Header = () => {
     return (
         <div className='header-container'>
             <div className='header-left'>
-                <Icon imageUrl="/icon.png" size={50}/>
+                <Icon icon={Menu} />
                 <text className='header-title'>しったか掲示板</text>
             </div>
             <AuthButton/>
