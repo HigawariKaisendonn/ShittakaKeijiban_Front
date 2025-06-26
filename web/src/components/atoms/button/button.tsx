47012ce50f64423399202d52fa6a1c6372f758b4
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-06-26 22:39:59 +0900

    Atomsのbutton,inputを親でclassNameを使えるようにする

--- web/src/components/atoms/button/button.tsx
+++ web/src/components/atoms/button/button.tsx
@@ -1,18 +1,22 @@
 import "./button.scss";
 import classNames from "classnames";
 
+type ButtonVariant = "primary" | "secondary" | "danger";
+
 interface ButtonProps {
-  variant: "primary" | "secondary" | "danger";
+  variant: ButtonVariant;
   children: React.ReactNode;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
 }
 
-export const Button = ({ children, onClick, variant = "primary"}: ButtonProps) => {
+export const Button = ({ children, onClick, variant = "primary", className }: ButtonProps) => {
   const buttonClass = classNames("button", {
     "button--primary": variant === "primary",
     "button--secondary": variant === "secondary",
     "button--danger": variant === "danger",
-  });
+  },
+  className);
   return (
     <button className={buttonClass} onClick={onClick}>
       {children}
--- web/src/components/atoms/input/input.tsx
+++ web/src/components/atoms/input/input.tsx
@@ -1,22 +1,26 @@
 import classNames from "classnames";
 import "./input.scss";
 
+type InputVariant = "default" | "active" | "longtext" | "error" | "disabled";
+
 interface InputProps {
-  variant?: "default" | "active" | "longtext" | "error" | "disabled";
-    type?: string;
-    placeholder?: string;
-    value?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  variant?: InputVariant;
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }
 
-export const Input = ({ variant = "default", type, placeholder, value, onChange }: InputProps) => {
+export const Input = ({ variant = "default", type, placeholder, value, onChange, className }: InputProps) => {
   const inputClass = classNames("input",{
     "input--default": variant === "default",
     "input--active": variant === "active",
     "input--error": variant === "error",
     "input--disabled": variant === "disabled",
     "input--longtext": variant === "longtext",
-  });
+  },
+  className);
   return (
     <input
     className={inputClass}
