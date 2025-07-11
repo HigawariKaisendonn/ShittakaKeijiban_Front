2611f75559673d8179671b49d15724f27b1431bd
tachibana <kd1364619@st.kobedenshi.ac.jp>
2025-07-11 13:41:58 +0900

    footerの追加

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Header } from '@/components/molecules/header/Header';
-import { HeroCard } from '@/components/organisms/HeroCard/HeroCard';
-
+import { HeroCard } from '@/components/organisms/heroCard/HeroCard';
+import { Footer } from '@/components/molecules/footer/Footer';
 
 export default function Home() {
   return (
       <>
         <Header />
         <HeroCard />
+        <Footer />
       </>
   );
 }
