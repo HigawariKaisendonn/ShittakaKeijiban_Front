6f5e9a4b69c43ba15e2a0c3a8ae630e175ee48c4
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-14 22:44:10 +0900

    ルーティング

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     // Storybookファイルをビルドから除外
     config.module.rules.push({
